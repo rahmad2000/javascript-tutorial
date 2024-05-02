c1be2ae3dcc89a1b0349b4507ed010f43972e7b7
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-02 14:20:20 -0500

    Logical NOT and Break

--- Video-3/sandbox.js
+++ Video-3/sandbox.js
@@ -80,3 +80,31 @@ if (password.length >= 12 && password.includes('@')){
 }
 
 
+//logical NOT (!)
+
+let user = false;
+
+if (user) {
+    console.log('you must be logged in to continue');
+}
+
+console.log(!true);
+console.log(!false);
+
+// break and continue
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for (let i = 0; i < scores.length; i++) {
+
+    if (scores[i] === 0) {
+        continue;
+    }
+
+    console.log('your score: ', scores[i]);
+
+    if (scores[i] === 100) {
+        console.log('congrats, you got the top score!');
+        break;
+    }
+}
