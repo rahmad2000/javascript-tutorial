30c4b4c74e0bddbfd9a4b0c380e1d0a1e7acdae6
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-02 08:50:46 -0500

    Variables

--- Video-2/sandbox.js
+++ Video-2/sandbox.js
@@ -1,4 +1,24 @@
-alert('Hello World!');
+// alert('Hello World!');
+
+// Console Logs to logs things into Console
 
 console.log(1);
 console.log(2);
+
+// Variables
+
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+// points = 50
+
+var score = 75;
+console.log(score);
