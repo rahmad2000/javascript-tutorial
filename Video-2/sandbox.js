e9e9451ed6d8c4261397a87ac418145629b7809e
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-02 09:45:30 -0500

    Operations

--- Video-2/sandbox.js
+++ Video-2/sandbox.js
@@ -68,3 +68,43 @@
 // // let result  = email.substring(4,10);
 // // let result = email.replace('a','A');
 // console.log(result);
+
+
+// Math Operations +, -, *, /, **, %
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+let result = radius % 3;
+let result2 = pi * radius**2;
+
+console.log(result);
+console.log(result2);
+
+// order of operation - B E D M A S
+
+let result3 = 5 * (10-3)**2;
+
+let likes = 10;
+likes++;
+likes--;
+
+likes +=10;
+likes -=5;
+likes *=4;
+likes /=2;
+
+console.log(likes)
+
+// NaN - not a number
+
+console.log(5 /'hi');
+
+// concatination
+
+let result4 = 'the dish has ' + likes + ' likes';
+
+console.log(result4);
+
