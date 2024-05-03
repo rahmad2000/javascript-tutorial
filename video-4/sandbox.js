2a84b240af9dc9a979a93d00b54928031db18415
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-03 08:53:27 -0500

    Function with Prameters

--- video-4/sandbox.js
+++ video-4/sandbox.js
@@ -1,13 +1,22 @@
-// function declaration
-function greet(){
-    console.log('Hello There');
+// // function declaration
+// function greet(){
+//     console.log('Hello There');
+// }
+
+// // function expression
+// const speak = function () {
+//     console.log('Good Day!');
+// };
+// greet();
+
+// speak()
+
+
+
+
+// 2. Function with parameters
+const greet = function(name ='John', time = 'Morning'){
+    console.log(`Hello, Good ${time} ${name}`);
 }
+greet('Adam');
 
-// function expression
-const speak = function () {
-    console.log('Good Day!');
-};
-greet();
-
-
-speak()
