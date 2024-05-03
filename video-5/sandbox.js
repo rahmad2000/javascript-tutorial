3dfa2fd2e52b28c175b225762a15773e27a98fcb
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-03 11:32:02 -0500

    Objects Methods

--- video-5/sandbox.js
+++ video-5/sandbox.js
@@ -3,9 +3,13 @@
 let user = {
     name: 'Tyrion',
     age: 31,
-    family: 'Lannister',
+    house: 'Lannister',
     location: 'Kings Landing',
-    title: ['Hand of the Queen', 'Master of Coin', 'Hand of the King']
+    title: ['Hand of the Queen', 'Master of Coin', 'Hand of the King'],
+    //Methods
+    status: function() {
+       return `${this.name} is Alive`;
+    }
 }
 
 console.log(user);
@@ -19,4 +23,6 @@ console.log(user.age);
 // console.log(user['name']);
 
 console.log(typeof user);
+console.log(user.status());
+
 // array of objects
