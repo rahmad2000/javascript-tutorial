e9774e16489bbe65477a1e1deb9a0413c5d320bf
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-03 08:43:52 -0500

    Function Declaration and Expression

---
+++ video-4/sandbox.js
@@ -0,0 +1,13 @@
+// function declaration
+function greet(){
+    console.log('Hello There');
+}
+
+// function expression
+const speak = function () {
+    console.log('Good Day!');
+};
+greet();
+
+
+speak()
