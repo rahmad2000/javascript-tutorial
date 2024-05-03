4fdf4b4701de8b6d972969aa2e795e3c593d2604
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-03 09:02:38 -0500

    Function with Return

--- video-4/sandbox.js
+++ video-4/sandbox.js
@@ -14,9 +14,22 @@
 
 
 
-// 2. Function with parameters
-const greet = function(name ='John', time = 'Morning'){
-    console.log(`Hello, Good ${time} ${name}`);
-}
-greet('Adam');
+// // 2. Function with parameters
+// const greet = function(name ='John', time = 'Morning'){
+//     console.log(`Hello, Good ${time} ${name}`);
+// }
+// greet('Adam');
+
+
+
+
+
+// 3. Function with return
+const calArea = function(radius){
+    return 3.14 * radius**2;
+};
+let area = calArea(3);
+console.log(area);
+
+
 
