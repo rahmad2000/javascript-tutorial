eceafb39b90d83d378b1074015b04f0e0d85a745
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-05-06 14:32:19 -0500

    Get Elements

--- Video-6/sandbox.js
+++ Video-6/sandbox.js
@@ -3,8 +3,25 @@
 //  console.log(para);
 
 
- const para = document.querySelectorAll('p');
- const errors = document.querySelectorAll('.error'); 
- console.log(errors);
+//  const para = document.querySelectorAll('p');
+//  const errors = document.querySelectorAll('.error'); 
+//  console.log(errors);
 
- console.log(para[2]);
+//  console.log(para[2]);
+
+
+
+const title =  document.getElementById('page-title');
+console.log(title);
+
+const errors = document.getElementsByClassName('error');
+
+console.log(errors);
+
+console.log(errors[0]);
+
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+
+const para = document.querySelector('p');
+console.log(para);
